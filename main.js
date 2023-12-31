aca80913b8c0a677bfdac72effe93d566233a968
DevAde <108113243+AdeMEDIA@users.noreply.github.com>
2023-12-31 22:12:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function makeWish() {
+           
+            let userWish = document.getElementById("wishInput").value;
+
+            alert("God will grant your wish, " + "But you have to put in Work this year Boss");           
+        }   
+        
+       
+       
+           // Script from CodewithRandom
